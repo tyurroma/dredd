21d969f531c298ff7813ad92a5cbc72109d88f5a
Honza Javorek <mail@honzajavorek.cz>
2018-12-18 12:31:07 +0100

    chore: add CircleCI config

--- scripts/commitlint.sh
+++ scripts/commitlint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-# Validates format of the commit messages on Travis CI
+# Validates format of the commit messages on CI
 
 
 set -e  # aborts as soon as anything returns non-zero exit status
 
 
-if [ ! -z "$TRAVIS" ]; then
+if [ ! -z "$CI" ]; then
   git remote set-branches origin master
   git fetch --unshallow --quiet
   git checkout master --quiet
   git checkout - --quiet
-  ./node_modules/.bin/commitlint-travis
+  ./node_modules/.bin/commitlint-circle
 else
   ./node_modules/.bin/commitlint --from=master
 fi
